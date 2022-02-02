dc1aebfe4370c8b1a08b4be49e6424972d684409
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-02-02 17:06:42 +0100

    CREATE and DROP DATABASE

---
+++ mon_cine.sql
@@ -0,0 +1,10 @@
+/* supprime la base de données existante*/
+DROP DATABASE `mon_cine`;
+
+
+/* crée la base de données */
+CREATE DATABASE IF NOT EXISTS mon_cine CHARACTER SET UTF8mb4 COLLATE utf8mb4_general_ci;
+
+
+/* utiliser la base */
+USE mon_cine;
